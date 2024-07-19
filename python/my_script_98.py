4f03efa18067f03d756bd23a4c7a493eee13e8bd
tempiltin <temurbekshukurov0707@gmail.com>
2024-07-20 00:00:00 +0500

    Commit on 2024-07-20T00:00:00

--- python/my_script_98.py
+++ python/my_script_98.py
@@ -1,8 +1,9 @@
-# Fibonacci function
-def fibonacci(n):
-    a, b = 0, 1
-    for _ in range(n):
-        print(a, end=" ")
-        a, b = b, a + b
-n = 10
-fibonacci(n)
+# Prime number checker
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+print(is_prime(29))
