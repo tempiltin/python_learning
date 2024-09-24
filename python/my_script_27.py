2809fdf5a6e0c2ad555e96ab5583d7f2460c5b13
tempiltin <temurbekshukurov0707@gmail.com>
2024-09-25 00:00:00 +0500

    Commit on 2024-09-25T00:00:00

--- python/my_script_27.py
+++ python/my_script_27.py
@@ -1,7 +1,8 @@
-# Random password generator
-import random
-import string
-def generate_password(length=12):
-    characters = string.ascii_letters + string.digits + string.punctuation
-    return ''.join(random.choice(characters) for _ in range(length))
-print(generate_password())
+# Fibonacci function
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+n = 10
+fibonacci(n)
