f285596a0842f2c683a8c7b3d062e1c73cade32c
tempiltin <temurbekshukurov0707@gmail.com>
2024-10-29 00:00:00 +0500

    Commit on 2024-10-29T00:00:00

--- python/my_script_16.py
+++ python/my_script_16.py
@@ -1,9 +1,8 @@
-# Bubble Sort
-def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-    return arr
-print(bubble_sort([5, 3, 8, 6, 2]))
+# Fibonacci function
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+n = 10
+fibonacci(n)
